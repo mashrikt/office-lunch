b6415fb94eee9583fa1e8fdb77763c6e08b41ecb
Tahzib Mashrik <mashrikt@gmail.com>
2021-10-26 03:51:59 +0600

    Setup django admin panel models

--- office_lunch/votes/admin.py
+++ office_lunch/votes/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vote, Winner
+
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_date', 'menu', 'user')
+    ordering = ('-id',)
+    readonly_fields = ('get_date',)
+
+    def get_date(self, obj):
+        return obj.menu.date
+    get_date.short_description = 'Date'
+    get_date.admin_order_field = 'menu__date'
+
+
+class WinnerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'menu', 'vote_count', )
+    ordering = ('-id',)
+    readonly_fields = ('date',)
+
+
+admin.site.register(Vote, VoteAdmin)
+admin.site.register(Winner, WinnerAdmin)
